9ef077b1ec841523c2915b9481f5a7744c7048bd
Bruce Wayne <admin@bige0.com>
2020-01-06 18:33:57 +0800

    Migrate from http://customdesktoplogo.wikidot.com/

---
+++ CustomDesktopLogo/AboutBox.cs
@@ -0,0 +1,171 @@
+﻿// Custom Desktop Logo 1.0 - By: 2008 Eric Wong
+// September 20th, 2008
+// Custom Desktop Logo is open source software licensed under GNU GENERAL PUBLIC LICENSE V3. 
+// Use it as you wish, but you must share your source code under the terms of use of the license.
+
+// Custom Desktop Logo allows you to create custom static and animated logos from PNG images.
+
+// Copyright (C) 2008 by Eric Wong. 
+// VideoInPicture@gmail.com
+// http://customdesktoplogo.wikidot.com
+// http://easyunicodepaster.wikidot.com
+// http://circledock.wikidot.com
+// http://videoinpicture.wikidot.com
+// http://webcamsignature.wikidot.com
+// http://windowextractor.wikidot.com
+
+// Uses AMS.Profile from http://www.codeproject.com/KB/cs/readwritexmlini.aspx for .ini file operations (Open source, non-specific license)
+// Uses hotkey selector component from http://www.codeproject.com/KB/miscctrl/systemhotkey.aspx (Open source, non-specific license)
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+using MemoryManagement;
+
+namespace CustomDesktopLogo
+{
+    partial class AboutBox : Form
+    {
+        MainForm theParent;
+
+        public AboutBox(MainForm parent)
+        {
+            theParent = parent;
+            InitializeComponent();
+        }
+
+        #region Assembly Attribute Accessors
+
+        public string AssemblyTitle
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                if (attributes.Length > 0)
+                {
+                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                    if (titleAttribute.Title != "")
+                    {
+                        return titleAttribute.Title;
+                    }
+                }
+                return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+            }
+        }
+
+        public string AssemblyVersion
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        public string AssemblyDescription
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+            }
+        }
+
+        public string AssemblyProduct
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyProductAttribute)attributes[0]).Product;
+            }
+        }
+
+        public string AssemblyCopyright
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+            }
+        }
+
+        public string AssemblyCompany
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyCompanyAttribute)attributes[0]).Company;
+            }
+        }
+        #endregion
+
+        private void AboutBox_Load(object sender, EventArgs e)
+        {
+            this.Text = MainForm.language.helpAbout.aboutWindowTitle;
+            this.labelProductName.Text = AssemblyProduct;
+            this.labelVersion.Text = String.Format("{0}", AssemblyVersion);
+            this.labelCopyright.Text = AssemblyCopyright;
+
+            linkLabelSupportForum.Text = MainForm.language.helpAbout.officialSupportForum;
+            linkLabelOfficialWebsite.Text = MainForm.language.helpAbout.officialWebsite;
+            donateLinkLabel.Text = MainForm.language.helpAbout.donateProjectDevelopment;
+            linkLabelEmailAuthor.Text = MainForm.language.helpAbout.emailAuthor;
+
+            programDescriptionLabel.Text = MainForm.language.helpAbout.programDescription;
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void linkLabelSupportForum_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.donationcoder.com/Forums/bb/index.php?board=240.0");
+        }
+
+        private void linkLabelOfficialWebsite_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://customdesktoplogo.wikidot.com/");
+        }
+
+        private void linkLabelEmailAuthor_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("mailto:VideoInPicture@gmail.com");
+        }
+
+        private void donateLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=VideoInPicture%40gmail%2ecom&lc=CA&item_name=Window%20Custom%20Desktop%20Logo%20Development&currency_code=CAD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted");
+        }
+
+        private void AboutBox_Activated(object sender, EventArgs e)
+        {
+            //MemoryUtility.ClearUnusedMemory();
+        }
+
+        private void AboutBox_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //MemoryUtility.ClearUnusedMemory();
+        }
+
+    }
+}
---
+++ CustomDesktopLogo/AboutBox.designer.cs
@@ -0,0 +1,254 @@
+﻿namespace CustomDesktopLogo
+{
+    partial class AboutBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutBox));
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.labelProductName = new System.Windows.Forms.Label();
+            this.logoPictureBox = new System.Windows.Forms.PictureBox();
+            this.labelCopyright = new System.Windows.Forms.Label();
+            this.aboutCreditsDescription = new System.Windows.Forms.TextBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.linkLabelSupportForum = new System.Windows.Forms.LinkLabel();
+            this.linkLabelOfficialWebsite = new System.Windows.Forms.LinkLabel();
+            this.linkLabelEmailAuthor = new System.Windows.Forms.LinkLabel();
+            this.labelVersion = new System.Windows.Forms.Label();
+            this.donateLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.programDescriptionLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 175F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.Controls.Add(this.labelProductName, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.logoPictureBox, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.labelCopyright, 1, 2);
+            this.tableLayoutPanel.Controls.Add(this.aboutCreditsDescription, 1, 8);
+            this.tableLayoutPanel.Controls.Add(this.okButton, 1, 9);
+            this.tableLayoutPanel.Controls.Add(this.linkLabelSupportForum, 1, 3);
+            this.tableLayoutPanel.Controls.Add(this.linkLabelOfficialWebsite, 1, 4);
+            this.tableLayoutPanel.Controls.Add(this.linkLabelEmailAuthor, 1, 6);
+            this.tableLayoutPanel.Controls.Add(this.labelVersion, 1, 1);
+            this.tableLayoutPanel.Controls.Add(this.donateLinkLabel, 1, 5);
+            this.tableLayoutPanel.Controls.Add(this.programDescriptionLabel, 1, 7);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(9, 9);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 10;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(698, 456);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // labelProductName
+            // 
+            this.labelProductName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelProductName.Location = new System.Drawing.Point(181, 0);
+            this.labelProductName.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
+            this.labelProductName.MaximumSize = new System.Drawing.Size(0, 17);
+            this.labelProductName.Name = "labelProductName";
+            this.labelProductName.Size = new System.Drawing.Size(514, 17);
+            this.labelProductName.TabIndex = 31;
+            this.labelProductName.Text = "Product Name";
+            this.labelProductName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // logoPictureBox
+            // 
+            this.logoPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logoPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("logoPictureBox.Image")));
+            this.logoPictureBox.Location = new System.Drawing.Point(3, 3);
+            this.logoPictureBox.Name = "logoPictureBox";
+            this.tableLayoutPanel.SetRowSpan(this.logoPictureBox, 10);
+            this.logoPictureBox.Size = new System.Drawing.Size(169, 450);
+            this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logoPictureBox.TabIndex = 30;
+            this.logoPictureBox.TabStop = false;
+            // 
+            // labelCopyright
+            // 
+            this.labelCopyright.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelCopyright.Location = new System.Drawing.Point(181, 52);
+            this.labelCopyright.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
+            this.labelCopyright.MaximumSize = new System.Drawing.Size(0, 17);
+            this.labelCopyright.Name = "labelCopyright";
+            this.labelCopyright.Size = new System.Drawing.Size(514, 17);
+            this.labelCopyright.TabIndex = 21;
+            this.labelCopyright.Text = "Copyright";
+            this.labelCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // aboutCreditsDescription
+            // 
+            this.aboutCreditsDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.aboutCreditsDescription.Location = new System.Drawing.Point(181, 240);
+            this.aboutCreditsDescription.Margin = new System.Windows.Forms.Padding(6, 3, 3, 3);
+            this.aboutCreditsDescription.Multiline = true;
+            this.aboutCreditsDescription.Name = "aboutCreditsDescription";
+            this.aboutCreditsDescription.ReadOnly = true;
+            this.aboutCreditsDescription.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.aboutCreditsDescription.Size = new System.Drawing.Size(514, 181);
+            this.aboutCreditsDescription.TabIndex = 23;
+            this.aboutCreditsDescription.TabStop = false;
+            this.aboutCreditsDescription.Text = resources.GetString("aboutCreditsDescription.Text");
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.okButton.Location = new System.Drawing.Point(620, 430);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 24;
+            this.okButton.Text = "&OK";
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // linkLabelSupportForum
+            // 
+            this.linkLabelSupportForum.AutoSize = true;
+            this.linkLabelSupportForum.Location = new System.Drawing.Point(178, 78);
+            this.linkLabelSupportForum.Name = "linkLabelSupportForum";
+            this.linkLabelSupportForum.Size = new System.Drawing.Size(238, 13);
+            this.linkLabelSupportForum.TabIndex = 25;
+            this.linkLabelSupportForum.TabStop = true;
+            this.linkLabelSupportForum.Text = "Official Support Forum - www.donationcoder.com";
+            this.linkLabelSupportForum.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.linkLabelSupportForum.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelSupportForum_LinkClicked);
+            // 
+            // linkLabelOfficialWebsite
+            // 
+            this.linkLabelOfficialWebsite.AutoSize = true;
+            this.linkLabelOfficialWebsite.Location = new System.Drawing.Point(178, 104);
+            this.linkLabelOfficialWebsite.Name = "linkLabelOfficialWebsite";
+            this.linkLabelOfficialWebsite.Size = new System.Drawing.Size(272, 13);
+            this.linkLabelOfficialWebsite.TabIndex = 26;
+            this.linkLabelOfficialWebsite.TabStop = true;
+            this.linkLabelOfficialWebsite.Text = "Official Website - http://customdesktoplogo.wikidot.com";
+            this.linkLabelOfficialWebsite.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.linkLabelOfficialWebsite.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelOfficialWebsite_LinkClicked);
+            // 
+            // linkLabelEmailAuthor
+            // 
+            this.linkLabelEmailAuthor.AutoSize = true;
+            this.linkLabelEmailAuthor.Location = new System.Drawing.Point(178, 156);
+            this.linkLabelEmailAuthor.Name = "linkLabelEmailAuthor";
+            this.linkLabelEmailAuthor.Size = new System.Drawing.Size(205, 13);
+            this.linkLabelEmailAuthor.TabIndex = 28;
+            this.linkLabelEmailAuthor.TabStop = true;
+            this.linkLabelEmailAuthor.Text = "E-mail Author - VideoInPicture@gmail.com";
+            this.linkLabelEmailAuthor.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.linkLabelEmailAuthor.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelEmailAuthor_LinkClicked);
+            // 
+            // labelVersion
+            // 
+            this.labelVersion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelVersion.Location = new System.Drawing.Point(181, 26);
+            this.labelVersion.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
+            this.labelVersion.MaximumSize = new System.Drawing.Size(0, 17);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(514, 17);
+            this.labelVersion.TabIndex = 0;
+            this.labelVersion.Text = "Version";
+            this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // donateLinkLabel
+            // 
+            this.donateLinkLabel.AutoSize = true;
+            this.donateLinkLabel.Location = new System.Drawing.Point(178, 130);
+            this.donateLinkLabel.Name = "donateLinkLabel";
+            this.donateLinkLabel.Size = new System.Drawing.Size(352, 13);
+            this.donateLinkLabel.TabIndex = 29;
+            this.donateLinkLabel.TabStop = true;
+            this.donateLinkLabel.Text = "Donate to the Project Development (PayPal) - VideoInPicture@gmail.com";
+            this.donateLinkLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.donateLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.donateLinkLabel_LinkClicked);
+            // 
+            // programDescriptionLabel
+            // 
+            this.programDescriptionLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.programDescriptionLabel.Location = new System.Drawing.Point(178, 182);
+            this.programDescriptionLabel.Name = "programDescriptionLabel";
+            this.programDescriptionLabel.Size = new System.Drawing.Size(517, 55);
+            this.programDescriptionLabel.TabIndex = 32;
+            this.programDescriptionLabel.Text = resources.GetString("programDescriptionLabel.Text");
+            // 
+            // AboutBox
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.okButton;
+            this.ClientSize = new System.Drawing.Size(716, 474);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutBox";
+            this.Padding = new System.Windows.Forms.Padding(9);
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AboutBox";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.AboutBox_Load);
+            this.Activated += new System.EventHandler(this.AboutBox_Activated);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AboutBox_FormClosed);
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.tableLayoutPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.Label labelVersion;
+        private System.Windows.Forms.Label labelCopyright;
+        private System.Windows.Forms.TextBox aboutCreditsDescription;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.LinkLabel linkLabelSupportForum;
+        private System.Windows.Forms.LinkLabel linkLabelOfficialWebsite;
+        private System.Windows.Forms.LinkLabel linkLabelEmailAuthor;
+        private System.Windows.Forms.LinkLabel donateLinkLabel;
+        private System.Windows.Forms.PictureBox logoPictureBox;
+        private System.Windows.Forms.Label labelProductName;
+        private System.Windows.Forms.Label programDescriptionLabel;
+    }
+}
